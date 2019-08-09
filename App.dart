c952e37f349e73d37ea3ac18a4deddcbbc058212
Mehdi Hassan Akash <32744268+mhakash@users.noreply.github.com>
2019-08-09 18:14:45 +0600

    Add files via upload

---
+++ App.dart
@@ -0,0 +1,172 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+
+// class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Baby Names',
+//      home: MyHomePage(),
+//    );
+//  }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//  @override
+//  _MyHomePageState createState() {
+//    return _MyHomePageState();
+//  }
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+
+//   TextEditingController nameInput;
+//   TextEditingController voteInput;
+
+//   @override
+//   void initState() {
+//     nameInput = TextEditingController();
+//     voteInput = TextEditingController();
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Baby Name Votes')),
+//       body: _buildBody(context),
+//       floatingActionButton: _addItem(),
+//     );
+//   }
+
+//   Widget _addItem(){
+//     return FloatingActionButton(
+//       tooltip: 'Add',
+//       child: Icon(Icons.add),
+//       onPressed: _itemInputAlert,
+//     );
+//   }
+
+//   _itemInputAlert() async{
+//     await showDialog<String>(
+//       context: context,
+//       builder: (BuildContext context){
+//         return AlertDialog(
+//           contentPadding: EdgeInsets.all(16),
+//           content: Column(
+//             children: <Widget>[
+//               Text("Enter the form"),
+//               Expanded(
+//                 child: TextField(
+//                   decoration: InputDecoration(labelText: 'Name*'),
+//                   autofocus: true,
+//                   controller: nameInput,
+//                 ),
+//               ),
+            
+//               Expanded(
+//                 child: TextField(
+//                   autofocus: true,
+//                   decoration: InputDecoration(labelText: 'Votes*'),
+//                   controller: voteInput,
+//                 ),
+//               ),
+//             ],
+//           ),
+//           actions: <Widget>[
+
+//             FlatButton(
+//               child: Text('add'),
+//               onPressed: (){
+//                 if(nameInput.text.isNotEmpty && voteInput.text.isNotEmpty){
+//                   //Firestore.instance.collection('baby').add;
+                  
+//                   Firestore.instance.collection('baby')
+//                     .add({"name": nameInput.text, "votes": int.parse(voteInput.text)})
+//                     .then((result)=>{
+//                       Navigator.of(context).pop(),
+//                       nameInput.clear(),
+//                       voteInput.clear(),
+//                     })
+//                     .catchError((err)=>print(err));
+//                 }
+//               },
+//             ),
+
+//             FlatButton(
+//               child: Text('cancel'),
+//               onPressed: (){
+//                 nameInput.clear();
+//                 voteInput.clear();
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//           ],
+//         );
+//       }
+//     );
+//   }
+
+//  Widget _buildBody(BuildContext context) {
+   
+//     //return _buildList(context, dummySnapshot);
+//     return StreamBuilder<QuerySnapshot>(
+//       stream: Firestore.instance.collection('baby').snapshots(),
+//       builder: (context,snapshot){
+//         if(!snapshot.hasData) return LinearProgressIndicator();
+//         return _buildList(context, snapshot.data.documents);
+//       },
+//     );
+//  }
+
+//  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+//    //print('\nhere\n');
+//    return ListView(
+//      padding: const EdgeInsets.only(top: 20.0),
+//      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+//    );
+//  }
+
+//  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+//     final record = Record.fromSnapshot(data);
+//     return Padding(
+//      key: ValueKey(record.name),
+//      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//      child: Container(
+//        decoration: BoxDecoration(
+//          border: Border.all(color: Colors.grey),
+//          borderRadius: BorderRadius.circular(5.0),
+//        ),
+//        child: ListTile(
+//          title: Text(record.name),
+//          trailing: Text(record.votes.toString()),
+//          onTap: () => Firestore.instance.runTransaction((transaction) async {
+//                       final freshSnapshot = await transaction.get(record.reference);
+//                       final fresh = Record.fromSnapshot(freshSnapshot);
+
+//                       await transaction
+//                           .update(record.reference, {'votes': fresh.votes + 1});
+//                     }),
+//        ),
+//      ),
+//    );
+//  }
+// }
+
+// class Record {
+//  final String name;
+//  final int votes;
+//  final DocumentReference reference;
+
+//  Record.fromMap(Map<String, dynamic> map, {this.reference})
+//      : assert(map['name'] != null),
+//        assert(map['votes'] != null),
+//        name = map['name'],
+//        votes = map['votes'];
+
+//  Record.fromSnapshot(DocumentSnapshot snapshot)
+//      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+//  @override
+//  String toString() => "Record<$name:$votes>";
+// }
---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+//import 'tableApp.dart';
+//import 'myApp.dart';
+//import 'src/App.dart';
+import 'test/testapp.dart';
+
+void main() => runApp(MyApp());
+
