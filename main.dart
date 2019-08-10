4936083dc1590e8bea3e82582ce4551f79284add
Mehdi Hassan Akash <32744268+mhakash@users.noreply.github.com>
2019-08-10 16:02:55 +0600

    delete

--- main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-//import 'tableApp.dart';
-//import 'myApp.dart';
-//import 'src/App.dart';
-import 'test/testapp.dart';
-
-void main() => runApp(MyApp());
-
